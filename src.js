1d595ac9927f5587d599316c2f1b649fd5f9b716
Jonathan McDaniel <JonathanTMcDaniel@gmail.com>
2017-11-03 16:42:03 -0400

    Adding load to liteQuery

--- src.js
+++ src.js
@@ -1,4 +1,24 @@
 
+
+
+// liteQuery Private Functions
+// ===================================================================
+var httpLoad = function(url, liteQueryInstance) {
+  // Credit to Joan and Félix Gagnon-Grenier on StackOverflow at question answer:
+  // https://stackoverflow.com/a/4033310
+  var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+          liteQueryInstance.html(xmlHttp.responseText);
+        }
+    }
+    xmlHttp.open("GET", url, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+
+
+// liteQuery Public Functions
+// ===================================================================
 var q = {
   nodeList:[],
   constructor(selector){
@@ -35,9 +55,15 @@ var q = {
       this.nodeList[i][property] = value;
     }
     return this;
+  },
+  load(url){
+    httpLoad(url, this);
+    return this;
   }
 }
 
+// liteQuery Interface
+// ===================================================================
 function _$(selector){
   return q.constructor(selector);
 }
